8b1ecd68a820290bfb05781ff2a17180234ad80a
jetfire679 <jetfire679@gmail.com>
2022-04-25 10:35:08 -0400

    updating public key

--- setup.tf
+++ setup.tf
@@ -8,7 +8,7 @@ provider "tls" {
 #Create key-pair for logging into EC2 in us-east-1
 resource "aws_key_pair" "webserver-key" {
   key_name   = "webserver-key"
-  public_key = file("~/.ssh/id_rsa.pub")
+  public_key = file("./files/id_rsa.pub")
 }
 
 #Get Linux AMI ID using SSM Parameter endpoint in us-east-1
